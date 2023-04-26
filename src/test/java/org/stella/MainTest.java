24d9778116a7adf7f874736d33c92795dcc56758
IrSokolova <i.sokolova@innopolis.university>
2023-04-26 20:19:56 +0300

    Week3

--- src/test/java/org/stella/MainTest.java
+++ src/test/java/org/stella/MainTest.java
@@ -12,11 +12,11 @@ class MainTest {
 
     @ParameterizedTest(name = "{index} Typechecking well-typed program {0}")
     @ValueSource(strings = {
-            "tests/well-typed/factorial.stella",
-            "tests/well-typed/squares.stella",
-            "tests/well-typed/higher-order-1.stella",
-            "tests/well-typed/increment_twice.stella",
-            "tests/well-typed/logical-operators.stella"})
+//            "tests/records/well-typed/records-1.stella",
+            "tests/references/well-typed/refs-1.stella",
+            "tests/references/well-typed/refs-2.stella",
+            "tests/references/well-typed/refs-3.stella"
+    })
     void testWellTyped(String filepath) throws Exception {
         String[] args = new String[0];
         final InputStream original = System.in;
@@ -28,22 +28,11 @@ class MainTest {
 
     @ParameterizedTest(name = "{index} Typechecking ill-typed program {0}")
     @ValueSource(strings = {
-            "tests/ill-typed/applying-non-function-1.stella",
-            "tests/ill-typed/applying-non-function-2.stella",
-            "tests/ill-typed/applying-non-function-3.stella",
-            "tests/ill-typed/argument-type-mismatch-1.stella",
-            "tests/ill-typed/argument-type-mismatch-2.stella",
-            "tests/ill-typed/argument-type-mismatch-3.stella",
-            "tests/ill-typed/bad-if-1.stella",
-            "tests/ill-typed/bad-if-2.stella",
-            "tests/ill-typed/bad-succ-1.stella",
-            "tests/ill-typed/bad-succ-2.stella",
-            "tests/ill-typed/bad-succ-3.stella",
-            "tests/ill-typed/shadowed-variable-1.stella",
-            "tests/ill-typed/undefined-variable-1.stella",
-            "tests/ill-typed/undefined-variable-2.stella",
-            "tests/ill-typed/bad-squares-1.stella",
-            "tests/ill-typed/bad-squares-2.stella"})
+//            "tests/records/ill-typed/bad-records-1.stella",
+            "tests/references/ill-typed/bad-refs-1.stella",
+            "tests/references/ill-typed/bad-refs-2.stella",
+            "tests/references/ill-typed/bad-refs-3.stella",
+    })
     void testIllTyped(String filepath) throws Exception {
         String[] args = new String[0];
         final FileInputStream fips = new FileInputStream(filepath);
